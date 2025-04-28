293611924b441eed8df1aa65b19428ffef22e6e5
Mohammad-Mehdi-Delavari <mahdidlavary@gmail.com>
2025-04-28 16:04:42 +0330

    init git in the project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <p class="text-5xl p-23.75 text-amber-400 font-bold underline">
+      Hello world!
+    </p>
+    </>
+  )
+}
+
+export default App
